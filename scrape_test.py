24be1e6cc8790db7503ce61e9cc5c7775781b4eb
Vikash G <vikashgraja@gmail.com>
2024-10-02 21:53:53 +0530

    Added Scraper

---
+++ scrape_test.py
@@ -0,0 +1,10 @@
+from web_scraper import ScrapeRedbus
+import pandas as pd
+
+url = "https://www.redbus.in/online-booking/west-bengal-transport-corporation"
+
+scraper = ScrapeRedbus(url)
+data = scraper.start_scrape()
+
+df = pd.DataFrame(data)
+df.to_csv('test.csv', index=False)
