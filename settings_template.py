56490c69b04c5b7b3e877a12bfb69fc879c7b8f3
Vikash G <vikashgraja@gmail.com>
2024-10-03 20:11:47 +0530

    Added settings template

---
+++ settings_template.py
@@ -0,0 +1,8 @@
+"""This is a template file for settings file when executing the program please
+    ensure to add a settings.py with the below values, also add settings.py to
+    .gitignore :).
+"""
+
+mysql_server  = ''
+mysql_user = ''
+mysql_password = ''
